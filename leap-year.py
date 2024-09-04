dca194a9660d1da05c7057d3a8a5abe1debee2b0
pacable <igor.mamaev1@gmail.com>
2024-09-04 21:57:43 +0300

    format

--- leap-year.py
+++ leap-year.py
@@ -26,18 +26,19 @@ def birthdays_count(d1, m1, y1, d2, m2, y2):
         pt_birthdays -= 1
     return pt_birthdays, vs_birthdays
 
+
 def birthdays_count2(d1, m1, y1, d2, m2, y2):
     if d1 == 29 and m1 == 2:
-        print("Special")
+        # print("Special")
         pt_birthdays = count_leap_years(y1, y2) + 1
         if m2 < m1 or (m2 == m1 and d2 < 29):
-            #if pt_birthdays > 0:
-            print("-1")
+            # if pt_birthdays > 0:
+            # print("-1")
             pt_birthdays -= 1
         return pt_birthdays, pt_birthdays
     if m2 < m1 or (m2 == m1 and d2 < d1):
-        print("less")
-        vs_birthdays =  y2 - y1
+        # print("less")
+        vs_birthdays = y2 - y1
         pt_birthdays = count_leap_years(y1, y2) + 1
         if m2 < 2 or (m2 == 2 and d2 < 29):
             pt_birthdays -= 1
@@ -47,5 +48,6 @@ def birthdays_count2(d1, m1, y1, d2, m2, y2):
         if m2 < 2 or (m2 == 2 and d2 < 29):
             pt_birthdays -= 1
     return pt_birthdays, vs_birthdays
+
 
 print(*birthdays_count2(*map(int, input().split('.')), *map(int, input().split('.'))))
