9276cfc2224371b0c743af07f8bcdf6da4e0cfd1
Anatoly Mamaev <amamaev@hpdeb.home.local>
2024-09-04 21:54:13 +0300

    version before

--- leap-year.py
+++ leap-year.py
@@ -12,7 +12,7 @@ def count_leap_years(y1, y2):
 
 
 def birthdays_count(d1, m1, y1, d2, m2, y2):
-    if d1 == 29 and m1 == 2:  # костыль, обрабатывающий случай др у Васи 29 февраля 
+    if d1 == 29 and m1 == 2:  # костыль, обрабатывающий случай др у Васи 29 февраля как у Пети
         vs_birthdays = count_leap_years(y1, y2)
         if not (leap_year(y2) and (m2 > 2 or (m2 == 2 and d2 >= 29))):
             vs_birthdays -= 1
@@ -26,20 +26,17 @@ def birthdays_count(d1, m1, y1, d2, m2, y2):
         pt_birthdays -= 1
     return pt_birthdays, vs_birthdays
 
-
 def birthdays_count2(d1, m1, y1, d2, m2, y2):
     if d1 == 29 and m1 == 2:
-        #print("Special")
-        pt_birthdays = count_leap_years(y1, y2)
-        if m2 < m1 or (m2 == m1 and d2 <= d1):
-            if pt_birthdays == 0:
-                # pt_birthdays -= 1
-                pass
-            else:
-                pt_birthdays += 1
+        print("Special")
+        pt_birthdays = count_leap_years(y1, y2) + 1
+        if m2 < m1 or (m2 == m1 and d2 < 29):
+            #if pt_birthdays > 0:
+            print("-1")
+            pt_birthdays -= 1
         return pt_birthdays, pt_birthdays
     if m2 < m1 or (m2 == m1 and d2 < d1):
-        #print("less")
+        print("less")
         vs_birthdays =  y2 - y1
         pt_birthdays = count_leap_years(y1, y2) + 1
         if m2 < 2 or (m2 == 2 and d2 < 29):
@@ -50,6 +47,5 @@ def birthdays_count2(d1, m1, y1, d2, m2, y2):
         if m2 < 2 or (m2 == 2 and d2 < 29):
             pt_birthdays -= 1
     return pt_birthdays, vs_birthdays
-
 
 print(*birthdays_count2(*map(int, input().split('.')), *map(int, input().split('.'))))
