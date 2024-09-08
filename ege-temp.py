be78673daca375526ff4e6f543dcea48bc865744
pacable <igor.mamaev1@gmail.com>
2024-09-08 10:03:18 +0300

    4,5,6 task

--- ege-temp.py
+++ ege-temp.py
@@ -1,8 +1,7 @@
 from itertools import product
 
 cnt = 0
-for i in product(["К", "Л", "Р", "Т"], repeat=4):
+for i in product(sorted(["А", "В", "Т", "О", "П"]), repeat=4):
     cnt += 1
-    if cnt == 67:
+    if ''.join(i) == 'ВАТА':
         print(cnt, i)
-        break
